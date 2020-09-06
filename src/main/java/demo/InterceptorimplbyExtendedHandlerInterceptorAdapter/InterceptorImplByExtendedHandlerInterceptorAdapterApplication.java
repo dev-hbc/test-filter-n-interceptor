d3331e9782068887fce887ed31d96ba3dd26df06
Hongbin <developer.hb.cai@gmail.com>
2020-09-06 01:40:30 -0400

    init by spring-starter

---
+++ src/main/java/demo/InterceptorimplbyExtendedHandlerInterceptorAdapter/InterceptorImplByExtendedHandlerInterceptorAdapterApplication.java
@@ -0,0 +1,13 @@
+package demo.InterceptorimplbyExtendedHandlerInterceptorAdapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorImplByExtendedHandlerInterceptorAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterceptorImplByExtendedHandlerInterceptorAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/InterceptorimplbyExtendedHandlerInterceptorAdapter/InterceptorImplByExtendedHandlerInterceptorAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.InterceptorimplbyExtendedHandlerInterceptorAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorImplByExtendedHandlerInterceptorAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
